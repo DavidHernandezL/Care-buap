844cedc78f85fd631974b136a3daba0d36cc24cd
David Hernandez <hl_david@outlook.com>
2023-11-26 09:20:55 -0600

    Config PWA

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,49 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
+  plugins: [
+    react(),
+    pluginRewriteAll(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Care Buap',
+        short_name: 'care-buap',
+        description:
+          'Aplicación para cuidar la salud mental de los alumnos de la Benemérita Universidad Autónoma de Puebla',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': '/src',
