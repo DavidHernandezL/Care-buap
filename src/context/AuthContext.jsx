1a0b7ed29b7841d9370c327ebcacb499bdbef523
David Hernandez <hl_david@outlook.com>
2023-11-27 11:54:04 -0600

    manejo de errores

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,10 +1,10 @@
 import { createContext, useState, useContext } from 'react';
 import { registerUserRequest } from '@services/user';
 import {
-  loginUserRequest,
-  verifyUserTokenRequest,
-  recoverPasswordRequest,
-  resetPasswordRequest,
+	loginUserRequest,
+	verifyUserTokenRequest,
+	recoverPasswordRequest,
+	resetPasswordRequest,
 } from '@services/auth';
 import Cookies from 'js-cookie';
 import { useEffect } from 'react';
@@ -12,119 +12,122 @@ import { useEffect } from 'react';
 export const AuthContext = createContext();
 
 export const useAuth = () => {
-  const context = useContext(AuthContext);
+	const context = useContext(AuthContext);
 
-  if (!context) {
-    throw new Error('useAuth must be used within an AuthProvider');
-  }
+	if (!context) {
+		throw new Error('useAuth must be used within an AuthProvider');
+	}
 
-  return context;
+	return context;
 };
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [errors, setErrors] = useState(null);
-  const [loading, setLoading] = useState(true);
+	const [user, setUser] = useState(null);
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [errors, setErrors] = useState(null);
+	const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setErrors(null);
-    }, 3000);
-    return () => clearTimeout(timer);
-  }, [errors]);
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setErrors(null);
+		}, 3000);
+		return () => clearTimeout(timer);
+	}, [errors]);
 
-  useEffect(() => {
-    const checkLogin = async () => {
-      const cookies = Cookies.get();
-      console.log(cookies);
-      console.log('cookies');
-      if (!cookies.token) {
-        setIsAuthenticated(false);
-        setLoading(false);
-        return;
-      }
+	useEffect(() => {
+		const checkLogin = async () => {
+			const cookies = Cookies.get();
+			console.log(cookies);
+			console.log('cookies');
+			if (!cookies.token) {
+				setIsAuthenticated(false);
+				setLoading(false);
+				return;
+			}
 
-      try {
-        const { data: res } = await verifyUserTokenRequest(cookies.token);
-        if (!res.data) return setIsAuthenticated(false);
-        setIsAuthenticated(true);
-        setUser(res.data);
-        setLoading(false);
-      } catch (error) {
-        setIsAuthenticated(false);
-        setLoading(false);
-      }
-    };
-    checkLogin();
-  }, []);
+			try {
+				const { data: res } = await verifyUserTokenRequest(cookies.token);
+				if (!res.data) return setIsAuthenticated(false);
+				setIsAuthenticated(true);
+				setUser(res.data);
+				setLoading(false);
+			} catch (error) {
+				setIsAuthenticated(false);
+				setLoading(false);
+			}
+		};
+		checkLogin();
+	}, []);
 
-  const signup = async (user) => {
-    try {
-      const { data: response } = await registerUserRequest(user);
-      const { data: userResponse } = response;
-      setUser(userResponse);
-      setIsAuthenticated(true);
-      setErrors(null);
-    } catch (error) {
-      setErrors({ msg: error.response.data.msg });
-    }
-  };
+	const signup = async user => {
+		try {
+			const { data: response } = await registerUserRequest(user);
+			const { data: userResponse } = response;
+			setUser(userResponse);
+			setIsAuthenticated(true);
+			setErrors(null);
+		} catch (error) {
+			setErrors({ msg: error.response.data.msg });
+		}
+	};
 
-  const signin = async (user) => {
-    try {
-      const { data: response } = await loginUserRequest(user);
-      const { data: userResponse } = response;
+	const signin = async user => {
+		try {
+			const { data: response } = await loginUserRequest(user);
+			const { data: userResponse } = response;
+			console.log(userResponse);
+			setUser(userResponse);
+			setIsAuthenticated(true);
+			setErrors(null);
+		} catch (error) {
+			if (error.code === 'ERR_NETWORK') {
+				console.log('Fallo en la conexión de internet');
+				setErrors({ msg: 'Fallo en la conexión de internet' });
+			}
+			setErrors({ msg: error.response.data.msg });
+		}
+	};
 
-      setUser(userResponse);
-      setIsAuthenticated(true);
-      setErrors(null);
-    } catch (error) {
-      setErrors({ msg: error.response.data.msg });
-    }
-  };
+	const logout = () => {
+		Cookies.remove('token');
+		setUser(null);
+		setIsAuthenticated(false);
+	};
 
-  const logout = () => {
-    Cookies.remove('token');
-    setUser(null);
-    setIsAuthenticated(false);
-  };
+	const recoverPassword = async email => {
+		try {
+			const { data: response } = await recoverPasswordRequest(email);
+			return response;
+		} catch (error) {
+			setErrors({ msg: error.response.data.msg });
+		}
+	};
 
-  const recoverPassword = async (email) => {
-    try {
-      const { data: response } = await recoverPasswordRequest(email);
-      return response;
-    } catch (error) {
-      console.log(error.response.data);
-      setErrors({ msg: error.response.data.msg });
-    }
-  };
+	const resetPassword = async (id, token, data) => {
+		try {
+			const { data: response } = await resetPasswordRequest(id, token, data);
+			return response;
+		} catch (error) {
+			console.log(error.response.data);
+			setErrors({ msg: error.response.data.msg });
+		}
+	};
 
-  const resetPassword = async (id, token, data) => {
-    try {
-      const { data: response } = await resetPasswordRequest(id, token, data);
-      return response;
-    } catch (error) {
-      console.log(error.response.data);
-      setErrors({ msg: error.response.data.msg });
-    }
-  };
-
-  return (
-    <AuthContext.Provider
-      value={{
-        signup,
-        signin,
-        user,
-        isAuthenticated,
-        errors,
-        loading,
-        logout,
-        recoverPassword,
-        resetPassword,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+	return (
+		<AuthContext.Provider
+			value={{
+				signup,
+				signin,
+				user,
+				isAuthenticated,
+				errors,
+				loading,
+				logout,
+				recoverPassword,
+				resetPassword,
+			}}
+		>
+			{children}
+		</AuthContext.Provider>
+	);
 };
